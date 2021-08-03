df074d9b7557ee66551d774b78e30710883eabe9
Jaehun Choi <87312401+jayjaehunchoi@users.noreply.github.com>
2021-08-03 20:13:27 +0900

    Update 28.java

--- 자바/basicalgorithm/28.java
+++ 자바/basicalgorithm/28.java
@@ -1,56 +1,45 @@
-// 7번 테스트케이스 실패
+import java.util.TreeMap;
 
-
-import java.util.HashMap;
-import java.util.Map.Entry;
-
-public class Main {
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+	    
+	// 혹시몰라서 일단 순서대로 key배열하려고..
+        TreeMap<Integer, Integer> tMap = new TreeMap<Integer, Integer>();
+        
+	    // 먼저 잃어버린 친구들을 map에 넣어준다.
+        for(int i = 0 ; i < lost.length ; i++) {
+        	tMap.put(lost[i],1);
+        }
+        
+	 // 여벌도 있으면서 잃어버린 친구들을 먼저 제외해준다, 
+	 //이때 reserve에서는 이후 계산에 방해되지 않게 -1로 선언
+        for(int i = 0 ; i < reserve.length; i++) {
+        	if(tMap.containsKey(reserve[i])) {
+        		tMap.replace(reserve[i], 0);
+        		reserve[i] = -1;
+        	}
+        }
+        
 	
-
-	public static void main(String[] args) {
-		 	int answer = 0;
-	        int count = 0;
-	        
-	        int[] reserve = {3,4,5};
-	        int[] lost = {2,3,4};
-	        int n = 5;
-	        HashMap<Integer, Integer> loster = new HashMap<Integer, Integer>();
-	        
-	        for(int i = 0 ; i < lost.length ; i++) {
-	        	loster.put(lost[i], 1);
-	        }
-	              
-	        for(int i = 0 ; i < reserve.length ; i++) {
-	        	
-	        	
-	        	if(loster.containsKey(reserve[i])) {
-		        	if (0 < loster.get(reserve[i])) {
-		        		loster.replace(reserve[i], 0);
-		         	}
-	        	}
-	        	
-	        	else if(loster.containsKey(reserve[i]-1) && 0 != loster.get(reserve[i]-1)) {
-		        	if (0 < loster.get(reserve[i]-1)) {
-		        		loster.replace(reserve[i]-1, 0);
-		        	}	        	
-	        	}
-		 	
-	        	else if(loster.containsKey(reserve[i]+1) && 0 != loster.get(reserve[i]+1)) {
-		        	if (0 < loster.get(reserve[i]+1)) {
-		        		loster.replace(reserve[i]+1, 0);
-		        	}
-	        	}
-	        }
-	        
-	        for(Entry<Integer, Integer> comP : loster.entrySet()) {
-	        	if(0 == comP.getValue()) {
-	        		
-	        		count++;
-	        	
-	        	}
-	        }
-	      
-	        System.out.println(n- (lost.length-count));
-      
+	    // +1번 -1번 을 빌려주는 작업
+        for(int i = 0 ; i < reserve.length; i++) {
+        	
+    		if(tMap.containsKey(reserve[i]-1) && tMap.get(reserve[i]-1) == 1 ) {
+        		tMap.replace(reserve[i]-1, 0);
+        	}
+        	
+        	else if(tMap.containsKey(reserve[i]+1) && tMap.get(reserve[i]+1) == 1 ) {
+        		tMap.replace(reserve[i]+1, 0);
+        	}
+        }
+        
+	    // 남아있는 Value만 n에서 빼면 정답
+        for(int m : tMap.values()) {
+        	answer -= m;
+        }
+        
+        
+        return answer;
     }
 }
