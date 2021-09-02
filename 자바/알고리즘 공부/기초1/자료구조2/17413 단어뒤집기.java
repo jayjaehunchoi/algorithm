4b8ebc85e671c0fe70b3355958980a7faf58dc51
Jaehun Choi <87312401+jayjaehunchoi@users.noreply.github.com>
2021-09-03 07:35:33 +0900

    Create 17413 단어뒤집기.java

---
+++ 자바/알고리즘 공부/기초1/자료구조2/17413 단어뒤집기.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+class Main{
+	  //규칙만 잘 걸어주면 되는 문제
+    // br, bw ,sb로 시간 단축
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+      String s = br.readLine();
+      List<String> aList = new ArrayList<String>();
+      String word = "";
+      boolean check = false;
+      for(int i = 0 ; i < s.length(); i++) {
+
+    	  if(s.charAt(i) == '>') {
+    		  word += '>';
+    		  aList.add(word);
+    		  check = false;
+    		  word = "";
+    		  continue;
+    	  }else if(s.charAt(i) == '<') {
+    		  aList.add(word);
+    		  word = "<";
+    		  check= true;
+    		  continue;
+    	  }
+    	  else if(s.charAt(i) == 32 && !check) {
+    		  aList.add(word);
+    		  word = "";
+    		  continue;
+    	  }else if(i == s.length()-1) {
+    		  word += s.charAt(i);
+    		  aList.add(word);
+    		  continue;
+    	  }
+    	  word += s.charAt(i);
+      }
+      
+      for(int i = 0 ; i < aList.size() ;i++) {
+    	  String w = "";
+    	  for(int j = aList.get(i).length()-1 ; j >= 0; j--) {
+    		  if(aList.get(i).charAt(j)=='>') {
+    			  sb.append(aList.get(i));
+    			  break;
+    		  }
+    		  w += aList.get(i).charAt(j);
+    		  if(j == 0) {
+    			  if(i < aList.size()-1 && aList.get(i+1).charAt(0) == '<') {
+    				  sb.append(w);
+    				  
+    			  }else {
+    				  w += " ";
+        			  sb.append(w);
+    			  }
+    			  
+    		  }
+    	  }
+    	  
+      }
+      bw.write(sb.toString());
+      bw.flush();
+      br.close();
+      bw.close();
+      
+      
+    }
+}
